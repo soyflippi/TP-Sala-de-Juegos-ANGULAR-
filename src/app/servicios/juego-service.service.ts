dc7345e3040f89e0f033fbfe23c1ecbe32a74973
soyflippi <soyflippi@gmail.com>
2019-09-09 16:52:39 -0300

    fix import problem

--- src/app/servicios/juego-service.service.ts
+++ src/app/servicios/juego-service.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Juego } from '../clases/juego';
 import { JuegoAdivina } from '../clases/juego-adivina';
-import { MiHttpService } from './mi-http/mi-http.service'; 
+import { MiHttpService } from './mi-http/mi-http.service';
 
 @Injectable()
 export class JuegoServiceService {
 
-  peticion:any;
-  constructor( public miHttp: MiHttpService ) {
+  peticion: any;
+  constructor(public miHttp: MiHttpService) {
     this.peticion = this.miHttp.httpGetO("http://localhost:3003");
-//    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
+    //    this.peticion = this.miHttp.httpGetO("https://restcountries.eu/rest/v2/all");
   }
 
   public listar(): Array<Juego> {
-   this.miHttp.httpGetP("https://restcountries.eu/rest/v2/all")
-    .then( data => {
-      console.log( data );
-    })
-    .catch( err => {
-      console.log( err );
-    });
-   
-  
+    this.miHttp.httpGetP("https://restcountries.eu/rest/v2/all")
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+
     this.peticion
-    .subscribe( data => {
-      console.log("En listar");
-      console.log( data );
-    }, err => {
-      console.info("error: " ,err );
-    })
+      .subscribe(data => {
+        console.log("En listar");
+        console.log(data);
+      }, err => {
+        console.info("error: ", err);
+      })
 
     let miArray: Array<Juego> = new Array<Juego>();
 
@@ -43,15 +43,15 @@ export class JuegoServiceService {
 
   public listarPromesa(): Promise<Array<Juego>> {
     this.peticion
-    .subscribe( data => {
-      console.log("En listarPromesa");
-      console.log( data );
-    }, err => {
-      console.log( err );
-    })
+      .subscribe(data => {
+        console.log("En listarPromesa");
+        console.log(data);
+      }, err => {
+        console.log(err);
+      })
     let promesa: Promise<Array<Juego>> = new Promise((resolve, reject) => {
       let miArray: Array<Juego> = new Array<Juego>();
-      miArray.push(new JuegoAdivina("JuegoPromesa 1", false,"promesa"));
+      miArray.push(new JuegoAdivina("JuegoPromesa 1", false, "promesa"));
       miArray.push(new JuegoAdivina("PepePromesa", true));
       miArray.push(new JuegoAdivina("JuegoPromesa 3", false));
       miArray.push(new JuegoAdivina("JuegoPromesa 4", false));
