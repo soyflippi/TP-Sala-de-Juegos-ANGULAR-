748d7dd6231fcdfa36806b138a0a9190482333f5
soyflippi <soyflippi@gmail.com>
2019-09-16 10:27:29 -0300

    Fix menu

--- src/app/componentes/menu-card/menu-card.component.ts
+++ src/app/componentes/menu-card/menu-card.component.ts
@@ -10,22 +10,22 @@ export class MenuCardComponent implements OnInit {
   constructor(private route: ActivatedRoute,
     private router: Router) { }
 
-
   ngOnInit() {
   }
+
   Juego(tipo: string) {
     switch (tipo) {
       case 'Adivina':
-          this.router.navigate(['/Juegos/Adivina']);
+        this.router.navigate(['/Juegos/Adivina']);
         break;
       case 'Agilidad':
-          this.router.navigate(['/Juegos/Agilidad']);
+        this.router.navigate(['/Juegos/Agilidad']);
         break;
       case 'AdivinaMasListado':
-          this.router.navigate(['/Juegos/AdivinaMasListado']);
+        this.router.navigate(['/Juegos/AdivinaMasListado']);
         break;
       case 'AgilidadaMasListado':
-          this.router.navigate(['/Juegos/AgilidadaMasListado']);
+        this.router.navigate(['/Juegos/AgilidadaMasListado']);
         break;
     }
   }
