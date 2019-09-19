8cb2ccf47f3f5cd0d1e2b0abc6750388456c0044
soyflippi <soyflippi@gmail.com>
2019-09-19 16:43:02 -0300

    Edit banner, add design menu games

--- src/app/servicios/archivos-jugadores.service.ts
+++ src/app/servicios/archivos-jugadores.service.ts
@@ -1,31 +1,23 @@
 import { Injectable } from '@angular/core';
-import { MiHttpService } from './mi-http/mi-http.service'; 
+import { MiHttpService } from './mi-http/mi-http.service';
 
 @Injectable()
 export class ArchivosJugadoresService {
 
-  api="http://localhost:8080/jugadoresarchivo/apirestjugadores/";
-  peticion:any;
-  constructor( public miHttp: MiHttpService ) {
-    
+  api = "http://localhost:8080/jugadoresarchivo/apirestjugadores/";
+  peticion: any;
+  constructor(public miHttp: MiHttpService) {
+
   }
-
-
-  public   traerJugadores(ruta) {
-    return this.miHttp.httpGetO(this.api+ruta)
-    .toPromise()
-    .then( data => {
-      console.log("Archivo jugadores");
-     // console.log( data );
-      return data;
-    }, err => {
-      console.log( err );
-    })
- 
-
-  
+  public traerJugadores(ruta) {
+    return this.miHttp.httpGetO(this.api + ruta)
+      .toPromise()
+      .then(data => {
+        console.log("Archivo jugadores");
+        // console.log( data );
+        return data;
+      }, err => {
+        console.log(err);
+      })
   }
-
-
-
 }
