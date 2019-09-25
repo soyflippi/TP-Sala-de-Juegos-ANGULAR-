addafb362a7c878e2f936f628b424c45157c49e0
soyflippi <soyflippi@gmail.com>
2019-09-25 13:53:13 -0300

    Add agilidad game

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,4 +1,71 @@
-export class JuegoAgilidad {
+import { Juego } from '../clases/juego'
+
+export class JuegoAgilidad extends Juego {
     numeroIngresado: number;
     gano: boolean;
+
+    maxNumber = 11;
+    n1 = 0;
+    n2 = 0;
+    question: string;
+    answer: number;
+    userAnswer: string;
+    lifePlayer: number = 0;
+    scorePlayer: number = 0;
+
+    constructor(nombre?: string, gano?: boolean, jugador?: string) {
+        super("Agilidad Artimetica", gano, jugador);
+    }
+
+    reloadPage() {
+        this.lifePlayer = 3;
+        this.scorePlayer = 0;
+        this.getNewQuestion();
+    }
+
+    getNewQuestion() {
+        this.n1 = Math.floor(Math.random() * this.maxNumber);
+        this.n2 = Math.floor(Math.random() * this.maxNumber);
+        this.question = this.n1 + " + " + this.n2;
+        this.answer = this.n1 + this.n2;
+        this.userAnswer = "";
+    }
+
+    onVoiceAnswer() {
+        if (this.userAnswer && parseInt(this.userAnswer) == this.answer) {
+            this.onRightAnswer();
+        }
+    }
+
+    public verificar() {
+        if (this.userAnswer && parseInt(this.userAnswer) == this.answer) {
+            return this.onRightAnswer();
+        } else {
+            return this.onWrongAnswer();
+        }
+    }
+
+    onRightAnswer() {
+        this.scorePlayer++;
+        this.getNewQuestion();
+        return true;
+    }
+
+    onWrongAnswer() {
+        this.lifePlayer--;
+        if (this.lifePlayer <= 0) {
+            this.gano = false;
+        }
+        return false;
+    }
+
+    skipQuestion() {
+        this.getNewQuestion();
+        this.scorePlayer--;
+    }
+
+    range(num) {
+        return new Array(num);
+    }
 }
+
