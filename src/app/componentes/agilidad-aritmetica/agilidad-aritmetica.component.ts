28718ba05578403b6084c67e91a0c5d944edd0b1
soyflippi <soyflippi@gmail.com>
2019-09-25 13:53:23 -0300

    Add agilidad game

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -13,40 +13,15 @@ import { TimerObservable } from "rxjs/observable/TimerObservable";
 export class AgilidadAritmeticaComponent implements OnInit {
   @Output() enviarJuego: EventEmitter<any> = new EventEmitter<any>();
   nuevoJuego: JuegoAgilidad;
-  ocultarVerificar: boolean;
-  Tiempo: number;
-  repetidor: any;
-  private subscription: Subscription;
+
 
   ngOnInit() {
   }
 
   constructor() {
-    this.ocultarVerificar = true;
-    this.Tiempo = 5;
     this.nuevoJuego = new JuegoAgilidad();
+    this.nuevoJuego.reloadPage();
     console.info("Inicio agilidad");
-  }
-
-  NuevoJuego() {
-    this.ocultarVerificar = false;
-    this.repetidor = setInterval(() => {
-
-      this.Tiempo--;
-      console.log("llego", this.Tiempo);
-      if (this.Tiempo == 0) {
-        clearInterval(this.repetidor);
-        this.verificar();
-        this.ocultarVerificar = true;
-        this.Tiempo = 5;
-      }
-    }, 900);
-
-  }
-
-  verificar() {
-    this.ocultarVerificar = false;
-    clearInterval(this.repetidor);
   }
 
 }
